9d8fd83a558bb6278f8e84277342b0168fc0c4cf
Mezzomaniac <Mezzomaniac@users.noreply.github.com>
2023-04-26 23:19:15 +0800

    aoc2017_2.2

--- adventofcode/aoc2017_2.py
+++ adventofcode/aoc2017_2.py
@@ -3,9 +3,18 @@ from downloader import download
 download(2017, 2)
 with open('aoc2017_2input.txt') as inputfile:
     data = inputfile.read()
+print(data)
 
-checksum = 0
+checksum1 = 0
+checksum2 = 0
 for row in data.splitlines():
     numbers = {int(n) for n in row.split()}
-    checksum += max(numbers) - min(numbers)
-print(checksum)
+    checksum1 += max(numbers) - min(numbers)
+    if len(numbers) != len(row.split()):
+        print('duplicated number')
+    for a in numbers:
+        for b in numbers:
+            if a != b and not a % b:
+                checksum2 += a // b
+print(checksum1)
+print(checksum2)
